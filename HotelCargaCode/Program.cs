f017efebe06d44796cfec46e64f99a2aa3b4f111
Pablo Corrales <devpablo.corrales@gmail.com>
2026-03-19 01:53:05 -0600

    Update assembly informational version and clean up unused namespaces

--- HotelCargaCode/Program.cs
+++ HotelCargaCode/Program.cs
@@ -4,8 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Linq;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
--- HotelCargaCode/obj/Debug/net10.0/HotelCarga.AssemblyInfo.cs
+++ HotelCargaCode/obj/Debug/net10.0/HotelCarga.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelCarga")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f4b14596434ba721ddb5b0ad727e508df480985")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ae7a76d5636ec8d63d22a36435ebc665de900b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelCarga")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelCarga")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
